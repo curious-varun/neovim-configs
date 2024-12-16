044812bea3bb044a37fce187d9dd923e75b45b5f
Varun <devvarunbhardwaj@gmail.com>
2024-12-16 14:40:26 +0530

    added: aurora theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -18,4 +18,15 @@ return {
     vim.cmd [[colorscheme tokyodark]]
   end,
 
+  -- adding aurora
+  -- lazy
+  {
+    'ray-x/aurora',
+    init = function()
+      vim.g.aurora_italic = 1
+      vim.g.aurora_transparent = 1
+      vim.g.aurora_bold = 1
+    end,
+  }
+
 }
